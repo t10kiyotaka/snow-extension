20cdff7e990b87926d5c50fb879a2ca4edc719fe
Kiyotaka <t10kiyotaka@gmail.com>
2021-04-06 08:36:45 +0900

    Make snowflake

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ const App = () => {
         >
           Learn React
         </a>
+        <div>Hello!</div>
       </header>
     </div>
   );
--- src/content.ts
+++ src/content.ts
@@ -1,2 +1,19 @@
 // This file is injected as a content script
-console.log("Hello from content script!")
+
+import "./content.css"
+
+const body = document.getElementsByTagName("body");
+
+const snowflakesContainer = document.createElement("div");
+snowflakesContainer.className = "snawflake";
+snowflakesContainer.setAttribute("aria-hidden", "true");
+
+const snowflake = document.createElement("div");
+snowflake.className = "snowflake";
+snowflake.innerHTML = "❆";
+
+for(let i=0; i<12; i++) {
+  snowflakesContainer.appendChild(snowflake.cloneNode(true));
+}
+
+body[0]?.prepend(snowflakesContainer);
